8b8398be58f3ac9a63eddff1e3924c8d911bdc3e
Thomas AWOUNFOUET <thomas.awounfouet@gmail.com>
2023-04-07 12:43:08 +0200

    Ajout du component Dynamic Text

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -2,14 +2,24 @@ import React from 'react';
 import Navigation from '../components/Navigation';
 import Mouse from '../components/Mouse';
 import SocialNetwork from '../components/SocialNetwork';
+import DynamicText from '../components/DynamicText';
 
 const Home = () => {
     return (
         <div>
-            <Navigation />
             <Mouse />
-            <SocialNetwork />
-            
+            <div className="home">
+                <Navigation />
+                <SocialNetwork />
+                <div className="home-main">
+                    <div className="main-content">
+                        <h1>FS AGENCY</h1>
+                        <h2>
+                            <DynamicText />
+                        </h2>
+                    </div>
+                </div>
+            </div>
         </div>
     );
 };
