9598fbf159bd3ff83cfa8272ceca1f5da16c19cf
Thomas AWOUNFOUET <thomas.awounfouet@gmail.com>
2023-04-07 11:32:16 +0200

    Ajout du component Mouse

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,10 +1,12 @@
 import React from 'react';
 import Navigation from '../components/Navigation';
+import Mouse from '../components/Mouse';
 
 const Home = () => {
     return (
         <div>
             <Navigation />
+            <Mouse />
             
         </div>
     );
